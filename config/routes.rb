b85a81261a52d2fecd9744520bf0daf1a879063b
websitekay <websitekay@gmail.com>
2017-02-25 21:30:28 -0800

    add User model and new action (user registration)

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'quests#index'
   resources :places, only: [:show]
   resources :quests, only: [:index]
+  resources :users, only: [:new, :create, :show]
 end
