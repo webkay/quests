afa6b4f17423993a8e2e1ffc62fde8c45cc5788d
websitekay <websitekay@gmail.com>
2017-02-26 15:10:13 -0800

    Add tests for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid with password confirmation match" do
+    user = User.new password: 'secret', password_confirmation: 'secret'
+    assert user.valid?
+  end
+
+  test "invalid with password confirmation not match" do
+    user = User.new password: 'secret', password_confirmation: 'different'
+    assert user.invalid?
+  end
 end
