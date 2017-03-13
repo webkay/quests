79a18dd3aa03856e649d07db8bc91d3dac944e5f
websitekay <websitekay@gmail.com>
2017-03-12 17:20:35 -0700

    Add signed_in? helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,18 @@ class ApplicationController < ActionController::Base
   end
 
   def require_login
-    session[:redirect_to_url] = request.url
-    redirect_to new_session_url unless current_user
+    unless signed_in?
+      session[:redirect_to_url] = request.url
+      redirect_to new_session_url
+    end
   end
 
   def require_logout
-    redirect_to root_url unless current_user.nil?
+    redirect_to root_url if signed_in?
   end
+
+  def signed_in?
+    session[:user_id].present?
+  end
+  helper_method :signed_in?
 end
